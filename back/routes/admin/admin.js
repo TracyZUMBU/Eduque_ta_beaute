8785de415606b74ec185a7f75b0c61076d4b0eb0
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-05-26 14:38:10 +0200

    added route for admin/recipe page

--- back/routes/admin/admin.js
+++ back/routes/admin/admin.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 //Retrieve the 10 last recipes
 router.get('/recipes', (req, res) => {
-    connection.query('SELECT * FROM recipes LIMIT 3' , (err, results) => {
+    connection.query('SELECT * FROM recipes ORDER BY created_at DESC LIMIT 3' , (err, results) => {
         if(err) {
             res.status(500).send('Error retrieving recipes')
             
